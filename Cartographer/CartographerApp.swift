4fefc36963cda20061052532c57dc122760cc8d6
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2023-02-23 16:17:02 -0600

    Initial Commit

---
+++ Cartographer/CartographerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CartographerApp.swift
+//  Cartographer
+//
+//  Created by Dr. Brandon Wiley on 2/23/23.
+//
+
+import SwiftUI
+
+@main
+struct CartographerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
